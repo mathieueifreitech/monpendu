5fd6d688a811992d2b3ba642af73befba3af17cf
Math <math@valneo.net>
2015-06-05 11:43:25 +0200

    data1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -39,34 +39,7 @@ $scope.showAlert = function() {
 })
 
 .controller('PagePlayCTRL', function($scope, $ionicModal, $ionicPopup, $timeout) {
-   
-   $scope.showPopup = function() {
-  $scope.data = {}
-
-  // An elaborate, custom popup
-  var myPopup = $ionicPopup.show({
-    template: '<input type="password" ng-model="data.wifi">',
-    title: 'Enter Wi-Fi Password',
-    subTitle: 'Please use normal things',
-    scope: $scope,
-    buttons: [
-      { text: 'Cancel' },
-      {
-        text: '<b>Save</b>',
-        type: 'button-positive',
-        onTap: function(e) {
-          if (!$scope.data.wifi) {
-            //don't allow the user to close unless he enters wifi password
-            e.preventDefault();
-          } else {
-            return $scope.data.wifi;
-          }
-        }
-      }
-    ]
-  });
-};
-
+ 
   $ionicModal.fromTemplateUrl('my-modal.html', {
     scope: $scope,
     animation: 'slide-in-up'
@@ -91,7 +64,7 @@ $scope.showAlert = function() {
       inputType: 'text',
       inputPlaceholder: 'Your Word',
       buttons: [
-      { text: 'Random' }, {text: 'ok'}
+      { text: 'Random', type: 'button-positive' }, {text: 'ok', onTap: function(e){}}
       ]
     }).then(function(res){
       console.log('Le mot est');
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,45 @@
 angular.module('starter.services', [])
 
+.factory('DatabaseService', function($cordovaSQLite) {
+  var Database = (function() {
+ 
+    // CONSTRUCTOR
+ 
+    function Database () {
+      
+    }
+ 
+    // ABSTRACT
+ 
+    Database.prototype.execute = function(query, params, cbSuccess, cbError) {
+ 
+    }
+ 
+    Database.prototype.create = function(table_name, fields_description) {
+    }
+ 
+    Database.prototype.insert = function(table_name, fields_ar, values_ar) {
+    }
+ 
+    Database.prototype.remove = function(table_name, callback) {
+    }
+ 
+    // ATTRIBUTES
+ 
+    Database.prototype.dbName          = 'HANGMAN.db'
+    Database.prototype.dbVersion       = '0.42'
+    Database.prototype.dbComments      = 'Efreitech'
+    Database.prototype.dbEstimatedSize = 200000
+ 
+    Database.prototype.db              = undefined
+ 
+    // Go
+    return Database;
+  })();
+ 
+  return new Database;
+})
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
